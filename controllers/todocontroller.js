6b2ad592ae37b648159f5995545fd0cdb85119a8
Marc Wafik <marc.wafik@hotmail.com>
2016-09-13 16:00:36 +0200

    Java Script
    -Use camel case in object functions and variables
    -Use augment
    -Alarm test
    -Single ton design pattern

---
+++ controllers/todocontroller.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var ToDoController = {
+	
+
+	/**
+	 * add a new to do item to databas
+	 * @param {String} description of the todo
+	 * @param {Int} time in seconds to fire the alarm of the reminder
+	 */
+	Add: function (description, reminder) {
+		/*var t = new ToDo(description, reminder);
+		t.save();*/
+	},
+	/**
+	 * List all to do items
+	 */
+	List: function () {
+		
+	}
+
+
+};
